67938c69ee5cc1c9bd69630655c08e603349390b
Minh Nguyen <melnguyen115@gmail.com>
2019-04-21 22:12:22 -0700

    api shit

--- golden.js
+++ golden.js
@@ -2,12 +2,46 @@
 
 (function() {
     window.onload = function() {
-        document.getElementById("loc").onclick = findCoords;
+        document.getElementById("loc").onclick = test;
     };
 
+    function test() {
+        let offset = new Date().getTimezoneOffset();
+        console.log(offset);
+    }
+
     function findCoords() {
-        let city = document.getElementById("city");
-        let state = document.getElementById("state");
+        let cityField = document.getElementById("city");
+        let ddstate = document.getElementById("state");
+        
+        let city = cityField.value;
+        let state = ddstate.options[ddstate.selectedIndex].value;
+
+        let url = "https://us1.locationiq.com/v1/search.php?key=eb122602cf386b&format=json" +
+        "&state=" + state + "&city=" + city;
+
+        fetch(url)
+            .then(checkStatus)
+            .then(function(responseText) {
+                let json = JSON.parse(responseText);
+                let lat = json[0]["lat"];
+                let lon = json[0]["lon"];
+                console.log(lat + " " + lon);
+                getData(lat, lon);
+                
+            });
+    }
+
+    function getData(lat, lon) {
+        let url = "https://api.sunrise-sunset.org/json?lat=" + lat +
+        "&lng=" + lon;
+
+        fetch(url)
+            .then(checkStatus)
+            .then(function(responseText) {
+                let json = JSON.parse(responseText);
+                console.log(json);
+            });
     }
 
     /** Checks the status of data requests to the web service. It returns an error message if
